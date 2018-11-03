ce341a58c1347d07d11b77fdffe23a639999c7f5
gibeulah <gibeulah@microsoft.com>
2018-11-04 00:32:52 +0530

    FirstCommit

---
+++ MinCostPath/MinCostPathProcessor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinCostPath
+{
+    class MinCostPathProcessor
+    {
+        int[,] matrix;
+
+        public MinCostPathProcessor(int[,] matrix) {
+            this.matrix = matrix;
+        }
+
+        public void GetMinCostPathInMatrix() {
+
+            Console.WriteLine("---------------The input Matrix-------");
+            PrintMatrix(matrix);
+
+            int[,] costMatrix = new int[matrix.GetLength(0) + 1, matrix.GetLength(1) + 1];
+
+            costMatrix[0, 0] = matrix[0, 0];
+
+            for (int i = 1; i < matrix.GetLength(0); i++) {
+                costMatrix[i, 0] = matrix[i, 0] + costMatrix[i - 1, 0];
+            }
+
+            for (int j = 1; j < matrix.GetLength(1); j++) {
+                costMatrix[0, j] = matrix[0, j] + costMatrix[0, j-1];
+            }
+
+            for (int i = 1; i < matrix.GetLength(0); i++) {
+                for (int j = 1; j < matrix.GetLength(1); j++) {
+                    costMatrix[i, j] = matrix[i, j] + 
+                        Math.Min(costMatrix[i-1,j],
+                        Math.Min(costMatrix[i, j-1], costMatrix[i-1, j-1])
+                        );
+                }
+            }
+
+            int rowsPenUltimateIndex = matrix.GetLength(0) - 1;
+            int columnsPenUltimateIndex = matrix.GetLength(1) - 1;
+
+
+            costMatrix[matrix.GetLength(0), matrix.GetLength(1)] = costMatrix[rowsPenUltimateIndex, columnsPenUltimateIndex];
+            
+            Console.WriteLine("The minimum cost is "
+                + costMatrix[matrix.GetLength(0), matrix.GetLength(1)]);
+
+            Console.WriteLine("-----------The Cost Matrix -------------");
+            PrintMatrix(costMatrix);
+        }
+
+
+        private void PrintMatrix(int[,] inputMatrix) {
+
+            int rows = inputMatrix.GetLength(0);
+            int columns = inputMatrix.GetLength(1);
+
+            Console.WriteLine("------------------------");
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    Console.Write(inputMatrix[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------------------");
+        }
+    }
+}
--- MinCostPath/Program.cs
+++ MinCostPath/Program.cs
@@ -6,7 +6,47 @@ namespace MinCostPath
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Min Cost Path!");
+            Console.WriteLine("--------------");
+
+            int[,] inputMatrix = GetInput();
+            if (inputMatrix != null)
+            {
+                MinCostPathProcessor minCostPathProcessor = new MinCostPathProcessor(inputMatrix);
+                minCostPathProcessor.GetMinCostPathInMatrix();
+            }
+            else {
+                Console.WriteLine("Please try again with valid inputs!");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int[,] GetInput() {
+            int[,] matrix = null;
+
+            Console.WriteLine("Enter the number of rows in the matrix");
+            try
+            {
+                int noOfRows = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the number of columsn in the matrix");
+                int noOfColumns = int.Parse(Console.ReadLine());
+                matrix = new int[noOfRows, noOfColumns];
+
+                for (int i = 0; i < noOfRows; i++) {
+                    Console.WriteLine("Enter the elements in row "+i +"separated by space");
+                    String rowelems = Console.ReadLine();
+                    String[] elements = rowelems.Split(' ');
+                    for (int j = 0; j < elements.Length; j++) {
+                        matrix[i, j] = int.Parse(elements[j]);
+                    }
+                }
+                
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is"+exception.Message);
+            }
+            return matrix;
         }
     }
 }
